7d797cea5ad5f45ed111113bc20f9b06d52b5f63
Wendell Arnald <wendell.arnald@gmail.com>
2020-08-22 00:23:32 +0100

    Include fixed list and display on screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:todo/models/item.dart';
 
 void main() => runApp(App());
 
@@ -16,17 +17,31 @@ class App extends StatelessWidget {
   }
 }
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  var items = new List<Item>();
+
+  HomePage() {
+    items = [];
+    items.add(Item(title: "Item 1", done: false));
+    items.add(Item(title: "Item 2", done: true));
+  }
+
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text("Flutter To Do"),
       ),
-      body: Container(
-        child: Center(
-          child: Text("Home - To Do"),
-        ),
+      body: ListView.builder(
+        itemCount: widget.items.length,
+        itemBuilder: (context, index) {
+          return Text(widget.items[index].title);
+        },
       ),
     );
   }
